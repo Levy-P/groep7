97a4d226335aff319ab7d05644768c85e888f278
Levy-P <144422852+Levy-P@users.noreply.github.com>
2024-02-13 13:58:40 +0100

    script comments

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,regular,500,600,700,800" rel="stylesheet">
     <link rel="stylesheet" href="global.css">
 
+    <!--voor <script> tags-->
 </head>
 
 <body>
