183efcadc6a7822b60aff31e5270f80099ff3783
Levy-P <144422852+Levy-P@users.noreply.github.com>
2024-02-14 15:22:36 +0100

    update componenten en test html
    Co-authored-by: MaryamAldainy <MaryamAldainy@users.noreply.github.com>

--- libs/components/header.php
+++ libs/components/header.php
@@ -1,16 +1,17 @@
 <?php
 echo "
 <header flex>
-    <nav flex>
-        <a href=\"index.php\">Home</a>
+    <img src=\"assets/icons/hamburger.png\" alt=\"Dropdown Menu\" class=\"dropdown\">
+    <article flex id=\"nav\">
+        <a href=\"index.php\"   flex><p>Home</p></a>
         <p>•</p>
-        <a href=\"ourmenu.php\">Our Menu</a>
+        <a href=\"ourmenu.php\" flex><p>Our Menu</p></a>
         <p>•</p>
-        <a href=\"gallery.php\">Gallery</a>
+        <a href=\"gallery.php\" flex><p>Gallery</p></a>
         <p>•</p>
-        <a href=\"aboutus.php\">About Us</a>
-    </nav>
-    <img src=\"assets/default/logo.png\" alt=\"Pizza Piazo's Logo\">
+        <a href=\"aboutus.php\" flex><p>About Us</p></a>
+    </article>
+    <figure id=\"logo\"></figure>
 </header>
 ";
 ?>
