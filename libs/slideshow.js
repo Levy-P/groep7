f8a10dc029a60615dcb0ee31d8bd7b90306ead45
Levy-P <144422852+Levy-P@users.noreply.github.com>
2024-02-14 15:23:15 +0100

    aanmaken javascript

--- libs/slideshow.js
+++ libs/slideshow.js
@@ -1,1 +1,22 @@
-// Voor de home pagina
+const loading_bar = document.getElementsByClassName('loading')[0].getElementsByTagName('figure')[0];
+var slideshow_slide, slideshow_progress = 0;
+
+setInterval(() => {
+    slideshow_progress++;
+
+    if (slideshow_progress > 0) {
+        if (slideshow_progress > 100) {
+            slideshow_progress = 0;
+            slideshow_slide++; slideshow_slide %= 3;
+            loading_bar.style.width = '0%';
+        } else {
+            loading_bar.style.width = slideshow_progress + '%';
+        }
+    }
+}, 75);
+
+function toSlide(slide) {
+    slide = Math.round(slide);
+    slideshow_progress = -25;
+    slideshow_slide = Math.min(2, Math.max(0, slide));
+}
